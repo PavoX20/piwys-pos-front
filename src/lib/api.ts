4cb62cd006aea556016c5c3e68580e1215214512
Pablo Sarzosa <pavosarzosa20@gmail.com>
2025-12-08 23:09:52 -0500

    feat: se actualiza para aceptar variables de entorno

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '@/store/authStore';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000', // O localhost, lo que te haya funcionado
+  baseURL: import.meta.env.VITE_API_BASE_URL, // O localhost, lo que te haya funcionado
 });
 
 // Interceptor de Request: Antes de salir, pega el token
